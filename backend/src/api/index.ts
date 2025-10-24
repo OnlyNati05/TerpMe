ccf9157e53fbc68b36b8f403a54b7d2b30872010
Natnael Abebe <natnaelabebe2005@gmail.com>
2025-10-24 17:22:37 -0400

    added patch to cors methods

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -37,7 +37,7 @@ app.use(
   cors({
     origin: allowedOrigin,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATHC", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
